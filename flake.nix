474008cf705986e8c7139197c3bccacf4dc2c560
pkharvey <kayharvey@protonmail.com>
2021-11-10 17:54:43 +0000

    flake: add home-manager to newtoncrosby

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:/nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       newtoncrosby = nixpkgs.lib.nixosSystem {    # this is the hostname = some func
         system = "x86_64-linux";
         modules = [
           (import ./hosts/newtoncrosby/configuration.nix)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.pasha = {
+              home.username = "pasha";
+              home.homeDirectory = "/home/pasha";
+            };
+          }
         ];
         specialArgs = { inherit inputs; };
       };
