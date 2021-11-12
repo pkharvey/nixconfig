2712cb1e746d3f15c2e1bf9cc4728a093ab83e1a
pkharvey <kayharvey@protonmail.com>
2021-11-12 03:19:38 +0000

    newtoncrosby: update hardware-configuration.nix
    Adds a swapfile to newtoncrosby via a zfs zvol

--- hosts/newtoncrosby/hardware-configuration.nix
+++ hosts/newtoncrosby/hardware-configuration.nix
@@ -8,9 +8,9 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
@@ -28,6 +28,8 @@
       fsType = "zfs";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/0ec42a05-baba-413b-b8b3-9716e100d384"; }
+    ];
 
 }
