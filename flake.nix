2d6a557942fff54553779b4c57c08c019f52c2eb
MatthewCroughan <matt@croughan.sh>
2021-11-14 04:36:24 +0000

    flake: add brownstone (#1)
    Brownstone is a Samsung Galaxy Note 3 LTE (N9005/P), codename hlte.
    
    A TWRP flashable zip can be built as follows:
    
    nix build .#robotnixConfigurations.brownstone.ota

--- flake.nix
+++ flake.nix
@@ -4,9 +4,18 @@
   inputs = {
     nixpkgs.url = "github:/nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    robotnix.url = "github:danielfullmer/robotnix";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, robotnix, ... }@inputs: {
+
+    # Applies the function `robotnixSystem` to each of the attributes in the
+    # set, for example `hlte`. This means I can have a set of phones to build.
+    robotnixConfigurations = nixpkgs.lib.mapAttrs (n: v: inputs.robotnix.lib.robotnixSystem v) {
+      brownstone = import ./hosts/brownstone/default.nix;
+      #anotherPhone = import ./hosts/anotherPhone/default.nix;
+    };
+
     nixosConfigurations = {
       newtoncrosby = nixpkgs.lib.nixosSystem {    # this is the hostname = some func
         system = "x86_64-linux";
