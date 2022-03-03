2dac18b60fb5cb060f947c274e90d9ebe59cba68
pkharvey <kayharvey@protonmail.com>
2022-03-03 05:45:20 +0000

    newtoncrosby: set environment variables for wayland

--- hosts/newtoncrosby/modules/sway.nix
+++ hosts/newtoncrosby/modules/sway.nix
@@ -1,6 +1,18 @@
 { pkgs, lib, config, ... }:
 
 {
+
+  environment.variables = {
+    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_USE_XINPUT2 = "1";
+    SDL_VIDEODRIVER = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "sway";
+  };
+
   environment.systemPackages = [
     pkgs.alacritty
   ];
