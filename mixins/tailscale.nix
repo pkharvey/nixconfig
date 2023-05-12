f7ce4545d9f948b0dcc53cf83356f22fa2f700b7
pkharvey <kayharvey@protonmail.com>
2023-05-12 01:48:29 +0100

    tailscale: force networkd-wait-online to be off

--- mixins/tailscale.nix
+++ mixins/tailscale.nix
@@ -1,5 +1,6 @@
+{ lib, ... }:
 {
-  systemd.services.systemd-networkd-wait-online.enable = false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
   networking = {
     useNetworkd = true;
     firewall = {
