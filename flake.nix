843c007e05a7e6624ce59d63c5424388a19122e8
aws abdulrahman <aous.20002017@gmail.com>
2023-08-20 22:23:15 +0200

    Cleaned Up flake.nix

--- flake.nix
+++ flake.nix
@@ -4,90 +4,33 @@
   inputs = {
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:/nixos/nixpkgs/nixos-unstable";
-    nixinate.url = "github:matthewcroughan/nixinate";
     home-manager.url = "github:nix-community/home-manager";
-    robotnix.url = "github:danielfullmer/robotnix";
-    firefox = {
-      url = "github:colemickens/flake-firefox-nightly";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, robotnix, firefox, nixinate, nixos-hardware, ... }@inputs: {
-
-    apps = nixinate.nixinate.x86_64-linux self;
-
-    # Applies the function `robotnixSystem` to each of the attributes in the
-    # set, for example `hlte`. This means I can have a set of phones to build.
-    robotnixConfigurations = nixpkgs.lib.mapAttrs (n: v: inputs.robotnix.lib.robotnixSystem v) {
-      brownstone = import ./hosts/brownstone/default.nix;
-      #anotherPhone = import ./hosts/anotherPhone/default.nix;
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }@inputs: {
     };
 
     nixosConfigurations = {
-      prodeskalpha = nixpkgs.lib.nixosSystem {    # this is the hostname = some func
+      aws = nixpkgs.lib.nixosSystem {    # this is the hostname = some func
         system = "x86_64-linux";
         modules = [
-          (import ./hosts/prodeskalpha/configuration.nix)
+          (import ./hosts/aws/configuration.nix)
           home-manager.nixosModules.home-manager
           nixos-hardware.nixosModules.common-cpu-intel
           {
             _module.args.nixinate = {
-              host = "prodeskalpha";
-              sshUser = "pasha";
-              buildOn = "remote";
+              host = "aws";
             };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.pasha = {
-              home.username = "pasha";
+            home-manager.users.aws= {
+              home.username = "aws";
               home.stateVersion = "22.11";
-              home.homeDirectory = "/home/pasha";
+              home.homeDirectory = "/home/aws";
             };
           }
         ];
         specialArgs = { inherit inputs; };
       };
-      newtoncrosby = nixpkgs.lib.nixosSystem {    # this is the hostname = some func
-        system = "x86_64-linux";
-        modules = [
-          (import ./hosts/newtoncrosby/configuration.nix)
-          home-manager.nixosModules.home-manager
-          {
-            _module.args.nixinate = {
-              host = "newtoncrosby";
-              sshUser = "pasha";
-              buildOn = "remote";
-            };
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pasha = {
-              home.username = "pasha";
-              home.stateVersion = "22.11";
-              home.homeDirectory = "/home/pasha";
-            };
-          }
-        ];
-        specialArgs = { inherit inputs; };
-      };
-      ptpx220 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (import ./hosts/ptpx220/configuration.nix)
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.pasha = {
-              home.username = "pasha";
-              home.stateVersion = "22.11";
-              home.homeDirectory = "/home/pasha";
-            };
-          }
-        ];
-        specialArgs = { inherit inputs; };
-      };
-    };
-  };
 }
 
