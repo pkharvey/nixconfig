7ab512aab494559c588decc3d9d6d6a25f3bfaaf
pkharvey <kayharvey@protonmail.com>
2023-12-29 12:25:52 +0000

    dslogic-udev.nix: package added: DSView

--- mixins/dslogic-udev.nix
+++ mixins/dslogic-udev.nix
@@ -3,4 +3,9 @@
     # DreamSourceLab DSLogic Logic Analyzer
     ATTRS{idVendor}=="2a0e", MODE="0666", ENV{ID_MM_DEVICE_IGNORE}="1", ENV{ID_MM_PORT_IGNORE}="1"
   '';
+  home-manager.users.pasha = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      dsview
+    ];
+  };
 }
